cf11e180b93757a35fb8d3d32f48be1f4b6f8768
xiaohao <sh2619978@126.com>
2013-04-12 19:02:16 +0800

    init

---
+++ src/veloeclipseext/editor/completion/SpringMvcCompletionProvider.java
@@ -0,0 +1,68 @@
+package veloeclipseext.editor.completion;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.contentassist.CompletionProposal;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+
+import com.googlecode.veloeclipse.editor.VelocityEditor;
+import com.googlecode.veloeclipse.editor.completion.ICompletionProvider;
+import com.googlecode.veloeclipse.vaulttec.ui.editor.text.VelocityTextGuesser;
+import com.googlecode.veloeclipse.vaulttec.ui.model.ITreeNode;
+import com.googlecode.veloeclipse.vaulttec.ui.model.ModelTools;
+
+public class SpringMvcCompletionProvider implements ICompletionProvider {
+    
+    private static Map<Integer, String> guesserTypeMap = new HashMap<Integer, String>();
+    
+    static {
+        try {
+            Field[] fields = VelocityTextGuesser.class.getFields();
+            VelocityTextGuesser object = VelocityTextGuesser.class.newInstance();
+            for (Field field : fields) {
+               guesserTypeMap.put(field.getInt(object), field.getName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Collection getExtraProposals(VelocityEditor editor, IFile file, IDocument doc, VelocityTextGuesser prefix,
+            int offset) throws CoreException {
+
+        if (prefix.getType() == VelocityTextGuesser.TYPE_VARIABLE) {
+//            return getVariableProposals(editor, prefix, offset);
+        } else if (prefix.getType() == VelocityTextGuesser.TYPE_MEMBER_QUALIFIER) {
+            
+        }
+        
+        return getVariableProposals(editor, prefix, offset);
+//        return new ArrayList<ICompletionProposal>();
+    }
+
+    private List<ICompletionProposal> getVariableProposals(VelocityEditor editor, VelocityTextGuesser prefix, int offset) {
+        ModelTools modelTools = new ModelTools(editor);
+        ITreeNode nodeByLine = modelTools.getNodeByLine(prefix.getLine());
+        
+        ITreeNode rootNode = editor.getRootNode();
+        ITreeNode lastRootNode = editor.getLastRootNode();
+        Object[] rootElements = editor.getRootElements();
+        
+        
+        List<ICompletionProposal> proposals = new ArrayList<ICompletionProposal>();
+//        String showText = "type=" + guesserTypeMap.get(prefix.getType()) + ";text=" + prefix.getText() + ";variable=" + prefix.getVariable() + ";line=" + prefix.getLine();
+        String showText = "test";
+        proposals.add(new CompletionProposal(showText, offset, prefix.getText().length(), showText.length(), null, showText, null, null));
+        return proposals;
+    }
+
+}
