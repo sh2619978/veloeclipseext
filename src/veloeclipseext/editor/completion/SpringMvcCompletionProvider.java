3839dd61bea47fb1e4ccc8a4d41a674af77cc5e7
xiaohao <sh2619978@126.com>
2013-04-16 19:05:58 +0800

    jdt search engine

--- src/veloeclipseext/editor/completion/SpringMvcCompletionProvider.java
+++ src/veloeclipseext/editor/completion/SpringMvcCompletionProvider.java
@@ -2,6 +2,7 @@ package veloeclipseext.editor.completion;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -9,10 +10,16 @@ import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.ILocalVariable;
+import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.search.IJavaSearchConstants;
 import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.core.search.SearchMatch;
+import org.eclipse.jdt.core.search.SearchParticipant;
 import org.eclipse.jdt.core.search.SearchPattern;
+import org.eclipse.jdt.core.search.SearchRequestor;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.contentassist.CompletionProposal;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
@@ -26,15 +33,15 @@ import com.googlecode.veloeclipse.vaulttec.ui.model.ITreeNode;
 import com.googlecode.veloeclipse.vaulttec.ui.model.ModelTools;
 
 public class SpringMvcCompletionProvider implements ICompletionProvider {
-    
+
     private static Map<Integer, String> guesserTypeMap = new HashMap<Integer, String>();
-    
+
     static {
         try {
             Field[] fields = VelocityTextGuesser.class.getFields();
             VelocityTextGuesser object = VelocityTextGuesser.class.newInstance();
             for (Field field : fields) {
-               guesserTypeMap.put(field.getInt(object), field.getName());
+                guesserTypeMap.put(field.getInt(object), field.getName());
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -46,35 +53,83 @@ public class SpringMvcCompletionProvider implements ICompletionProvider {
             int offset) throws CoreException {
 
         if (prefix.getType() == VelocityTextGuesser.TYPE_VARIABLE) {
-//            return getVariableProposals(editor, prefix, offset);
+            // return getVariableProposals(editor, prefix, offset);
         } else if (prefix.getType() == VelocityTextGuesser.TYPE_MEMBER_QUALIFIER) {
-            
+
         }
-        
+
         return getVariableProposals(editor, prefix, offset);
-//        return new ArrayList<ICompletionProposal>();
+        // return new ArrayList<ICompletionProposal>();
     }
 
-    private List<ICompletionProposal> getVariableProposals(VelocityEditor editor, VelocityTextGuesser prefix, int offset) {
+    private List<ICompletionProposal> getVariableProposals(VelocityEditor editor, final VelocityTextGuesser prefix,
+            final int offset) throws CoreException {
+        final List<ICompletionProposal> proposals = new ArrayList<ICompletionProposal>();
+
         ModelTools modelTools = new ModelTools(editor);
         ITreeNode nodeByLine = modelTools.getNodeByLine(prefix.getLine());
-        
+
         ITreeNode rootNode = editor.getRootNode();
         ITreeNode lastRootNode = editor.getLastRootNode();
         Object[] rootElements = editor.getRootElements();
-        
+
         JdtManager jdtManager = new JdtManager();
         IJavaProject firstJavaProject = jdtManager.getFirstJavaProject();
-        
-        SearchPattern pattern = SearchPattern.createPattern("java.lang.Object()", IJavaSearchConstants.METHOD, IJavaSearchConstants.QUALIFIED_REFERENCE, SearchPattern.R_PATTERN_MATCH);
+        if (!firstJavaProject.isOpen()) {
+            firstJavaProject.open(null);
+        }
+
         SearchEngine engine = new SearchEngine();
+        SearchPattern pattern = SearchPattern.createPattern("java.util.Date", IJavaSearchConstants.TYPE,
+                IJavaSearchConstants.PARAMETER_DECLARATION_TYPE_REFERENCE, SearchPattern.R_EXACT_MATCH);
+
+        final List<IMethod> methodList = new ArrayList<IMethod>();
+        SearchRequestor requestor = new SearchRequestor() {
+
+            @Override
+            public void acceptSearchMatch(SearchMatch match) throws CoreException {
+                IJavaElement element = (IJavaElement) match.getElement();
+                // String showText = "type=" + guesserTypeMap.get(prefix.getType()) + ";text=" + prefix.getText() +
+                // ";variable=" + prefix.getVariable() + ";line=" + prefix.getLine();
+                String showText = (element.getElementType() == IJavaElement.METHOD ? "method  " : "  ")
+                        + element.toString();
+                proposals.add(new CompletionProposal(showText, offset, prefix.getText().length(), showText.length(),
+                        null, showText, null, null));
+                if (element instanceof IMethod) {
+                    methodList.add((IMethod) element);
+                }
+            }
+        };
+
+        engine.search(pattern, new SearchParticipant[] { SearchEngine.getDefaultSearchParticipant() }, SearchEngine
+                .createJavaSearchScope(new IJavaElement[] { firstJavaProject.findPackageFragmentRoot(firstJavaProject
+                        .getPath().append("/src")) }), requestor, null);
+
         
+        for (IMethod method : methodList) {
+            if (!method.getElementName().equals("function")) {
+                continue;
+            }
+            SearchRequestor secondRequestor = new SearchRequestor() {
+
+                @Override
+                public void acceptSearchMatch(SearchMatch match) throws CoreException {
+                    IJavaElement element = (IJavaElement) match.getElement();
+                    System.out.println("offset="+ match.getOffset() + ";length=" + match.getLength());
+                }
+            };
+            
+            engine.search(SearchPattern.createPattern(method.getParameters()[0],
+                    IJavaSearchConstants.REFERENCES),
+                    new SearchParticipant[] { SearchEngine.getDefaultSearchParticipant() }, SearchEngine
+                            .createJavaSearchScope(new IJavaElement[]{method}), secondRequestor, null);
+        }
         
-        List<ICompletionProposal> proposals = new ArrayList<ICompletionProposal>();
-//        String showText = "type=" + guesserTypeMap.get(prefix.getType()) + ";text=" + prefix.getText() + ";variable=" + prefix.getVariable() + ";line=" + prefix.getLine();
-        String showText = "test";
-        proposals.add(new CompletionProposal(showText, offset, prefix.getText().length(), showText.length(), null, showText, null, null));
         return proposals;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
     }
 
 }
